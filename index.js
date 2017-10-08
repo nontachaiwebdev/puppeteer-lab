fe9eb349c59284ca50877320ee3e92efb09a1bdc
nontachaiwebdev <sutininter@gmail.com>
2017-10-09 00:35:46 +0700

    feat(test): Add code to test flow

--- index.js
+++ index.js
@@ -11,15 +11,34 @@ const puppeteer = require('puppeteer');
 
   const DELAY_OPTIONS = {
     waitUntil: 'networkidle',
-    networkIdleTimeout: '5000'
+    networkIdleTimeout: 2000
   }
+
+  const CLICK_OPTIONS = {
+    button: 'left',
+    clickCount: 1,
+    delay: 0
+  }
+
+  const amountOfAdult = 1
+
+  const URL = 'https://www.phuketplan.com/tour/detail/59c4d8540dabb59ed371d2c4'
 
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.setViewport(VIEWPORT)
-  await page.goto('https://www.phuketwide.com/', DELAY_OPTIONS);
-  await page.mouse.click(100, 250, {button: 'left'})
-  await page.screenshot({path: 'example.png'});
-
+  await page.goto(URL);
+  await page.waitForNavigation(DELAY_OPTIONS)
+  await page.click('.firstBottomBarBtn___33xag');
+  await page.waitForNavigation(DELAY_OPTIONS);
+  for(let i = 0 ; i <= amountOfAdult ; i++){
+    await page.click('.fa-plus', CLICK_OPTIONS);
+  }
+  await page.click('.sc-jDwBTQ', CLICK_OPTIONS);
+  await page.waitForNavigation(DELAY_OPTIONS);
+  await page.click('input[name="firstname"]', CLICK_OPTIONS);
+  await page.keyboard.sendCharacter('N', {delay: 100});
+  await page.click('input[name="lastname"]', CLICK_OPTIONS);
+  //await page.screenshot({path: 'example.png'});
   //await browser.close();
 })();
